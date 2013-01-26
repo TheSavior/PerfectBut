9e96a8fb7a2fd8fca3a23af995c9047da471441e
Eli White <git@eli-white.com>
2013-01-26 15:30:40 -0800

    Adding span ids

--- Application/Themes/Default/Controllers/Main/Index/index.php
+++ Application/Themes/Default/Controllers/Main/Index/index.php
@@ -1,18 +1,15 @@
 <div id ="TheWall">
     <h2><a href="thewall.html">The Wall</a></h2>
-
     <?php
-
         foreach ($this->Posts as $post) {
         ?>
         <div class ="singlepost">
             <span class="posttext">He's perfect but <?php echo $post->text?></span>
-            <a class="author"><?php echo $post->poster->username ?></a>
-            <a class="upvote"><?php echo $post->upvote?></a>
-            <a class="downvote"><?php echo $post->downvote?></a>
+            <span class="author"><?php echo $post->poster->username ?></span>
+            <span class="upvote" id="<?php echo $post->id?>_up"><?php echo $post->upvote?></span>
+            <span class="downvote" id="<?php echo $post->id?>_down"><?php echo $post->downvote?></span>
         </div>
         <?php
         }                                    
     ?>
-    </div>
 </div>
