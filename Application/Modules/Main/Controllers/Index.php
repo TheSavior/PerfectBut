137299ed0f24def348e0a9f26fe9dfc8af6bed39
Eli White <git@eli-white.com>
2013-01-26 13:39:07 -0800

    No longer have to be logged in to see the main page

--- Application/Modules/Main/Controllers/Index.php
+++ Application/Modules/Main/Controllers/Index.php
@@ -4,13 +4,6 @@ namespace Application\Modules\Main\Controllers;
 class Index extends \Saros\Application\Controller
 {
     public function init() {
-        $auth = \Saros\Auth::getInstance();
-        
-        if(!$auth->hasIdentity())
-        {      
-            // If we also don't have an identitiy, then we are not logged in at all
-            $this->redirect($GLOBALS["registry"]->utils->makeLink("Register", "index"));
-        }
     }
     
 	public function indexAction()
