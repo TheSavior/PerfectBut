62d8f6b4a434fd7d38ef84ce1564cbef087afd28
Eli White <git@eli-white.com>
2013-01-26 15:03:40 -0800

    Moving the script in

---
+++ Application/Themes/Default/Scripts/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function()    {
+    $(document).scroll(function(){
+        if (($(document).height() - $(window).height() - $(document).scrollTop()) < 100) {
+           loadPosts();
+        }
+    });
+});
+
+
+function loadPosts()    {
+    for(var i = 0 ; i < 10 ; i++)    {
+            var $newPost = $('<div>').addClass('singlepost');
+            $('<a>').addClass('posttext').text("This dude has a small dick nah mean son, it was very very very small    This dude has a small dick nah mean son, it was very very very small This dude has a small dick nah mean son, it was very very very small This dude has a small dick nah mean son, it was very very very small").appendTo($newPost);
+            $newPost.appendTo('TheWall');
+    }    
+}
