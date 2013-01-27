646de03f99cc8d879e2ed8e7d9a11aa3b1b26870
David Ewald <ewadav@uw.edu>
2013-01-26 16:20:14 -0800

    NO!

--- Application/Themes/Default/Scripts/index.js
+++ Application/Themes/Default/Scripts/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 header("Content-type: application/x-javascript");
 ?>
@@ -45,4 +46,46 @@ function updateVoteCount()  {
             dataType: 'jsonp' ,
             success: printVoteCount 
     });   
+=======
+var url = "http://localhost/PerfectBut/Api/Posts/getAll";
+
+
+
+$(document).ready(function()    {
+        $('upvote, downvote').click(updateVoteCount);
+
+        loadPosts();
+});
+
+
+function printPosts (data)  {
+     alert(data);
+     $.each(data, function(i){
+           
+            var resource = data.response[i];
+            var $newPost = $('<div>').addClass('singlepost').appendTo('TheWall')
+            $('<a>').addClass('posttext').text(resource.text).appendTo($newPost);
+            $('<p>').addClass('upvote').text(resource.upvotes).appendTo($newPost);
+            $('<p>').addClass('downvote').text(resource.downvotes).appendTo($newPost);
+            $('<p>').addClass('author').text(resource.username).appendTo($newPost);
+            $('<p>').addClass('time').text(resource.timestamp).appendTo($newPost);
+ 
+        });
+}
+
+function loadPosts()    {
+    $.ajax({
+            url: url ,
+            dataType: 'jsonp' ,
+            success: printPosts
+    });
+}
+
+function updateVoteCount()  {
+     $.ajax({
+            url: url ,
+            dataType: 'jsonp' ,
+            success: printVoteCount 
+    });   
+>>>>>>> ...
 }
