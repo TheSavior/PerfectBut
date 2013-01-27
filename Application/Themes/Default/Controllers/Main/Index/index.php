692522b3e4adead6d7113d95a0bd2bf603987e1b
Eli White <git@eli-white.com>
2013-01-27 14:33:22 -0800

    Adding bigger images, cleaning it up

--- Application/Themes/Default/Controllers/Main/Index/index.php
+++ Application/Themes/Default/Controllers/Main/Index/index.php
@@ -1,13 +1,19 @@
-<!--<h2>The Wall</h2>
+<!--<h2>The Wall</h2>-->
 
-<ul id="nav">
-    <li>Most Recent</li>
-    <li>Top Posts</li>
-    <li>Location</li>
-    <li>My Posts</li>
-    <li>Favorites</li>
-</ul>
--->
+<div id="ads">
+    <!-- Begin: adBrite, Generated: 2013-01-27 2:11:07  -->
+    <style type="text/css">
+       .adHeadline {font: bold 10pt Arial; text-decoration: underline; color: #0000FF;}
+       .adText {font: normal 10pt Arial; text-decoration: none; color: #000000;}
+    </style>
+    <script type="text/javascript">
+    var AdBrite_Page_Url = '';
+    try{var AdBrite_Iframe=window.top!=window.self?2:1;var AdBrite_Referrer=document.referrer==''?document.location:document.referrer;AdBrite_Referrer=encodeURIComponent(AdBrite_Referrer);}catch(e){var AdBrite_Iframe='';var AdBrite_Referrer='';}
+    document.write(String.fromCharCode(60,83,67,82,73,80,84));document.write(' src="http://ads.adbrite.com/mb/text_group.php?sid=2280247&br=1&dk=63617220696e737572616e63655f355f325f776562&ifr='+AdBrite_Iframe+'&ref='+AdBrite_Referrer+'&purl='+encodeURIComponent(AdBrite_Page_Url)+'" type="text/javascript">');document.write(String.fromCharCode(60,47,83,67,82,73,80,84,62));</script>
+    <div><a class="adHeadline" target="_top" href="http://www.adbrite.com/mb/commerce/purchase_form.php?opid=2280247&afsid=1">Your Ad Here</a></div>
+    <!-- End: adBrite -->
+</div>
+
 <div id ="TheWall">
     <?php
         foreach ($this->Posts as $post) {
@@ -23,12 +29,12 @@
                 <div class="ratings">
                     <span class="voteOption upvote" id="<?php echo $post->id?>_up">
                         <span>(<?php echo $post->upvote?>)</span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/ring.png"/>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/ring_big.png"/>
                         <span>Take Him</span>
                     </span>
                     <span class="voteOption downvote" id="<?php echo $post->id?>_down">
                         <span>(<?php echo $post->downvote?>)</span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/run.png"/>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/run_big.png"/>
                         <span>Leave Him</span>
                     </span>
                 </div>
