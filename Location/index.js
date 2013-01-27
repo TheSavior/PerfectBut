b5ee43a0defcd1cd447a05950a1430d75933b06f
Ashish Chandwani <Ashish.s.chandwani@gmail.com>
2013-01-26 22:46:06 -0800

    eli's blood sweat tears

--- Location/index.js
+++ Location/index.js
@@ -1,28 +1,72 @@
+"use strict";
+var MAPS = "http://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&sensor=false";
+var IP_GET = "http://jsonip.com/";
+var IP_TO_LOC = "http://www.geoplugin.net/json.gp";
+var locationVal;
+
 $(document).ready(function() {
-	getPermission(); // call this when/before posting
+	//getPermission(); // call this when/before posting
+	ajaxIP();
 });
 
 function getPermission() {
 	if (Modernizr.geolocation) {
 		navigator.geolocation.getCurrentPosition(getPosition,getPositionFailure);	// Callback is getPostion, gets called after user gives permission	
 	} else {
-		// no native support; set location value = IP
+		
 	
 	}
+}
+
+function ajaxIP() {
+	$.ajax({
+		dataType: "jsonp",
+		url: IP_GET,
+		success: getLocFromIP
+	});
+}
+
+function getLocFromIP(ajax) {
+	var ip = ajax.ip
+	$.ajax({
+		dateType: "jsonp",
+		url: IP_TO_LOC,
+		data: {"ip" : ip},
+		success: locFinder
+	});
 }
 
 /*					x:y
 input type="hidden" name="location" value="IP" id="postButton" />
 */
 
+function locFinder(ajax) {
+	console.log(ajax);
+}
+
 // Change postButton's value to 
 function getPosition(position) {
-	var lat = position.latitude;
-	var longitude = position.longitude;
 
+	var lat = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	locationVal = (lat + "," + longitude);
+	$.ajax({
+		dataType: "jsonp",
+		url: MAPS,
+		success: getCity
+
+	});
 	//$(#postButton).value = 
-	var locationVal = (lat + ":" + longitude);
 	console.log(locationVal);
+	console.log(position);
+
+}
+
+// Can be used to get different types of 
+function getCity(response) {
+	console.log(response.results);
+	var city = response.results[3].address_components[4].long_name;
+	console.log(city);
 }
 
 function getPositionFailure (err) {
