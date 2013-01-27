a6b238835b02558026c99485527a6052d5458f09
Eli White <git@eli-white.com>
2013-01-27 14:18:08 -0800

    Move the take/like into the main bubble

--- Application/Themes/Default/Controllers/Main/Index/index.php
+++ Application/Themes/Default/Controllers/Main/Index/index.php
@@ -20,27 +20,26 @@
             <div class="posttext">
                 <span class="intro">He's the perfect guy but...</span><br />
                 <span class="text"><?php echo $post->text?></span>
-                <div style="float: right;">
+                <div class="ratings">
+                    <span class="voteOption upvote" id="<?php echo $post->id?>_up">
+                        <span>(<?php echo $post->upvote?>)</span>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/ring.png"/>
+                        <span>Take Him</span>
+                    </span>
+                    <span class="voteOption downvote" id="<?php echo $post->id?>_down">
+                        <span>(<?php echo $post->downvote?>)</span>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/run.png"/>
+                        <span>Leave Him</span>
+                    </span>
+                </div>
+                <!--<div style="float: right;">
                 <?php $fbUrl = $GLOBALS["registry"]->utils->makeLink("Index", "viewPost", $post->id); ?>
                     <div class="fb-like" data-href="<?php echo $fbUrl; ?>" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
-                </div>
+                </div>-->
             </div>
             <div class="underpost">
                 Submitted
                 <span class="time"><?php echo \Application\Classes\Utils::formatDate($post->date_created); ?></span>
-                <!--in
-                <span class="location"><?php echo $city?></span>
-                    -->
-                <div class="ratings">
-                    <span class="voteOption upvote" id="<?php echo $post->id?>_up">
-                        <span><?php echo $post->upvote?></span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/heart.png"/>
-                    </span>
-                    <span class="voteOption downvote" id="<?php echo $post->id?>_down">
-                        <span><?php echo $post->downvote?></span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/heart_broken.png"/>
-                    </span>
-                </div>
             </div>
         </div>
         <?php
--- Application/Themes/Default/Controllers/Main/Index/viewPost.php
+++ Application/Themes/Default/Controllers/Main/Index/viewPost.php
@@ -25,9 +25,19 @@
             <div class="posttext">
                 <span class="intro">He's the perfect guy but...</span><br />
                 <span class="text"><?php echo $post->text?></span>
+                <div class="ratings">
+                    <span class="voteOption upvote" id="<?php echo $post->id?>_up">
+                        <span><?php echo $post->upvote?></span>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/ring.png"/>
+                    </span>
+                    <span class="voteOption downvote" id="<?php echo $post->id?>_down">
+                        <span><?php echo $post->downvote?></span>
+                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/run.png"/>
+                    </span>
+                </div>
                 <div style="float: right;">
                     <div class="fb-like" data-href="http://perfectguybut.com/Index/viewPost/2" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
-                </div>
+                </div> 
             </div>
             <div class="underpost">
                 Submitted
@@ -35,16 +45,7 @@
                 <!--in
                 <span class="location"><?php echo $city?></span>
                     -->
-                <div class="ratings">
-                    <span class="voteOption upvote" id="<?php echo $post->id?>_up">
-                        <span><?php echo $post->upvote?></span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/heart.png"/>
-                    </span>
-                    <span class="voteOption downvote" id="<?php echo $post->id?>_down">
-                        <span><?php echo $post->downvote?></span>
-                        <img src="<?php echo $_SERVER["ROOT"]?>Application/Themes/Default/Images/heart_broken.png"/>
-                    </span>
-                </div>
+                
             </div>
         </div>
         <?php 
