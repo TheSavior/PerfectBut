b50c02a60fd778e7b59b5c0b9a993648315f4512
Eli White <git@eli-white.com>
2013-01-30 00:50:02 -0800

    Can now like auto updated posts

--- Application/Themes/Default/Scripts/postLoader.js
+++ Application/Themes/Default/Scripts/postLoader.js
@@ -10,10 +10,14 @@ function createPost(data) {
     
     newpost.attr("id", data.timestamp);
     newpost.find(".text").text(data.text);
-    var upvote = newpost.find(".upvote").attr("id", data.id+"_up");
+    var upvote = newpost.find(".upvote").attr("id", data.id+"_up").click(function() {
+        updateVoteCount("up" , $(this))
+    });
     upvote.find(".votecount").attr("id", data.id+"_upcount").text("("+data.upvotes+")");
                                                                  
-    var downvote = newpost.find(".downvote").attr("id", data.id+"_down");
+    var downvote = newpost.find(".downvote").attr("id", data.id+"_down").click(function() {
+        updateVoteCount("up" , $(this))
+    });
     downvote.find(".votecount").attr("id", data.id+"_downcount").text("("+data.downvotes+")");
     
     newpost.find(".time").text(data.whenstring);
