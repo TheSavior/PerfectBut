32288e0c55b4cd0533789135366e0323b9f8e467
Eli White <git@eli-white.com>
2013-01-29 20:49:12 -0800

    Export posts

--- Application/Modules/Api/Controllers/Stats.php
+++ Application/Modules/Api/Controllers/Stats.php
@@ -13,4 +13,22 @@ class Stats extends \Saros\Application\Controller
         $this->view->show(false); ;
         echo $GLOBALS["registry"]->mapper->all('\Application\Entities\Posts')->count();
     }
+    
+    public function getPostsAction($limit = 0, $offset = null) {
+        $this->view->show(false);
+        
+        if ($limit != 0)
+            $limit = intval($limit);
+            
+        if ($offset != null)
+            $offset = intval($offset);
+            
+        header("Content-Type: text/plain", true);
+                
+        $items = array();
+        $posts = $GLOBALS["registry"]->mapper->all('\Application\Entities\Posts')->order(array("date_created"=>"desc"))->limit($limit, $offset);
+        foreach($posts as $post) {
+            echo $post->text."\n";
+        }
+    }
 }
